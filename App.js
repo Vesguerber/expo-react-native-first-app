79f9e162674fddfd90d911b01a1a93e5c813f152
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-08-30 05:16:26 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('I\'m gonna fight \'em all');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('A seven nation army couldn\'t hold me back')} />
+
       <StatusBar style="auto" />
     </View>
   );
